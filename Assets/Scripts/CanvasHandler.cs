433f64c723af1de6b8eb5eb6db4995adc4661890
Razedd <poll.jonker@hotmail.com>
2016-01-30 12:12:23 +0100

    ScoreHandler and basic UI

---
+++ Assets/Scripts/CanvasHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CanvasHandler : MonoBehaviour {
+    public Text score1;
+    public Text score2;
+    public Text score3;
+    public Text score4;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        score1.text = ScoreHandler.getInstance().getScore(1)+"";
+        score2.text = ScoreHandler.getInstance().getScore(2)+"";
+        score3.text = ScoreHandler.getInstance().getScore(3)+"";
+        score4.text = ScoreHandler.getInstance().getScore(4)+"";
+    }
+
+
+}
