2c72ae27615b769c0f1029a1a724d43b8bbec639
Valentinas Rimeika <valentinasr8901@gmail.com>
2016-01-30 11:28:11 +0100

    *Beat Structures

---
+++ Assets/Scripts/BeatAction.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeatAction 
+{
+	public Vector2 barSize;
+	public Sprite beatSprite;
+	private BeatBarBehaviour logic;
+
+	public BeatAction()
+	{
+		logic = new BeatBarBehaviour();
+	}
+}
---
+++ Assets/Scripts/Song.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Song 
+{
+	public AudioClip audioClip;
+	public List<BeatAction> beatArray;
+	public int beatsPerMinute;
+
+	public Song()
+	{
+		Log.Tinas( "Create Song object" );
+	}
+
+}
+ 
