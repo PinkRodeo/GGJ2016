e3248b73b980022ce79ae023ef433f6decd0e5f3
Weikie <weikie_yeh@hotmail.com>
2016-01-30 22:07:24 +0100

    +Did credit screen

--- Assets/Scripts/Flow/CreditSceneMaster.cs
+++ Assets/Scripts/Flow/CreditSceneMaster.cs
@@ -1,15 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class CreditSceneMaster : MonoBehaviour {
-
+public class CreditSceneMaster : MonoBehaviour
+{
+	private ControllerInput input;
+	private int endState;
+	public Image canvasImage;
+	public Sprite sponsorSprite;
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		input = new ControllerInput(1);
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+		if (input.AnyButtonPressed())
+		{
+			switch (endState)
+			{
+			case 0:
+				//show GGJ sponsor
+				canvasImage.sprite = sponsorSprite;
+				break;
+			default:
+				SceneManager.LoadScene(0);
+				break;
+			}
+			++endState;
+		}
 	}
 }
