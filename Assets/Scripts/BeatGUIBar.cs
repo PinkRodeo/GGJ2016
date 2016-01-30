2d46fef4442985fb7553edd839288c69da710adb
Valentinas Rimeika <valentinasr8901@gmail.com>
2016-01-30 21:04:14 +0100

    *Something, something, Beat bar gui

--- Assets/Scripts/BeatGUIBar.cs
+++ Assets/Scripts/BeatGUIBar.cs
@@ -4,23 +4,27 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using System;
+using System.Timers;
 
 public class BeatGUIBar : MonoBehaviour 
 {
-	public float barSpeed;
-	public float offset;
-	public int BeatsPerMinute;
-	public float delay;
-	public int startingAfter;
-	public List<BarType> pattern;
-	private int counter;
-	private int changeOnEveryOther;
-	private int patternIndex;
+	public float barSpeed = 300.0f;
+	public int BeatsPerMinute = 128;
+	public float DelayForMusic = 3.9f;
+	public int startingAfter = 15;
+
+	private float timer;
+	private float globalTime;
+
+	private int spawnIndex = 0;
+	private int currentIndex = 0;
+	private int beatLength = 0;
+
 	private Color barColor;
 	private GameObject canvas;
 	private AudioSource source;
 
-	public BarType[] beatList;
+	public Beat[] beatList;
 
 	public enum BarType
 	{
@@ -29,56 +33,92 @@ public class BeatGUIBar : MonoBehaviour
 		empty
 	}
 
-	BarType type;
+	public struct Beat
+	{
+		public float time;
+		public BarType type;
+	};
 
 	void Start () 
 	{
-		
-		type = BarType.normal;
+		timer = 60.0f / BeatsPerMinute;
 		canvas = GameObject.Find("Canvas");
-		//Invoke( "AddBeatBar", 60.0f / BeatsPerMinute );
 		source = GetComponent<AudioSource>();
-		//GenerateArray( source.clip.length );
+		GenerateArray( source.clip.length );
 		if( source != null )
 		{
-			source.PlayDelayed( delay );
+			source.PlayDelayed( DelayForMusic );
 		}
-		changeOnEveryOther = startingAfter;
-		patternIndex = -1;
-		barColor = Color.white;
 	}
 
 	void Update () 
 	{
-		
+		globalTime += Time.deltaTime;
+
+		while( currentIndex < beatLength )
+		{
+			if (beatList[currentIndex].time <= globalTime)
+			{
+				//we hit the beat in this frame so do some stuff
+				Log.Tinas("BEAT HITSZ@!" + currentIndex);
+				currentIndex++;
+			}
+			else 
+			{
+				break;
+			}
+		}
+		/*
+		while(spawnIndex < beatLength )
+		{
+			if (beatList[spawnIndex].time <= globalTime + DelayForMusic)
+			{
+				//This beat will be hit in 'delay' seconds so we spawn it now
+				spawnIndex++;
+			} 
+			else 
+			{
+				break;
+			}
+		}
+		*/
 
 	}
+		
 
 	void GenerateArray( float seconds )
 	{
-		float beatsPerSecond = 60.0f / BeatsPerMinute;
-		int size = (int)( seconds / beatsPerSecond  );
-		beatList = new BarType[ size ];
-		int amount = size - (size / 6);
-		for( int i = 0; i < size; i++ )
+		float beatsPerSecond = 1.0f / timer;
+		beatLength = (int)( seconds * beatsPerSecond  );
+		beatList = new Beat[ beatLength ];
+		int amount = beatLength - (beatLength / 8);
+		for( int i = 0; i < beatLength; i++ )
 		{
 			if ( i >= amount)
 			{
-				beatList[ i ] = BarType.empty;
-			}
-			if( Convert.ToBoolean(i & 3) )
-			{
-				beatList[ i ] = BarType.special;
-				beatList[ i + 1 ] = BarType.empty;
-			}
-			else
-			{
-				beatList[ i ] = BarType.normal;
-			}
+				Beat b = beatList[ i ];
+				b.type = BarType.empty;
+				beatList[ i ] = b;
+			} 
+			Beat c = beatList[ i ];
+			c.type = BarType.normal;
+			c.time = DelayForMusic + i * timer;
+			beatList[ i ] = c;
+		}
+
+		for( int i = 6 + startingAfter; i < beatLength; i += 8 )
+		{
+			Beat b = beatList[ i ];
+			b.type = BarType.special;
+			beatList[ i ] = b;
+
+			Beat c = beatList[ i + 1 ];
+			c.type = BarType.empty;
+			beatList[ i + 1  ] = c;
 		}
 
 
-		for( int i = 0; i < size; i++ )
+		for( int i = 0; i < beatLength; i++ )
 		{
 			GameObject bar = new GameObject( "beatBar", typeof( RectTransform ) );
 			bar.AddComponent<CanvasRenderer>();
@@ -88,74 +128,15 @@ public class BeatGUIBar : MonoBehaviour
 			bar.transform.SetParent( canvas.transform );
 			Vector3 p = rt.position;
 
-			rt.transform.position = new Vector3( p.x + Screen.currentResolution.width + i * 100.0f, p.y, p.z );
+			rt.transform.position = new Vector3( p.x + Screen.currentResolution.width + i * 300.0f, p.y, p.z );
 
 			BeatBarBehaviour behaviour = bar.AddComponent<BeatBarBehaviour>();
 			behaviour.barSpeed = barSpeed;
-			BarType a = beatList[ i ];
-			if( a == BarType.normal ) barColor = Color.blue;
-			if( a == BarType.special ) barColor = Color.red;
-			if( a == BarType.empty ) barColor = Color.yellow;
+			Beat a = beatList[ i ];
+			if( a.type == BarType.normal ) barColor = Color.blue;
+			if( a.type == BarType.special ) barColor = Color.red;
+			if( a.type == BarType.empty ) barColor = Color.yellow;
 			bar.GetComponent<CanvasRenderer>().SetColor( barColor );
 		}
-	}
-
-	int GetCount( BarType type )
-	{
-		int count = -1;
-		switch( type )
-		{
-			case BarType.normal:
-				count = 5;
-				break;
-			case BarType.special:
-				count = 0;
-				break;
-			case BarType.empty:
-				count = 0;
-				break;
-			default:
-				break;
-		}
-		return count;
-	}
-
-	void GetPatternType( GameObject bar )
-	{
-		if ( pattern[ patternIndex ] == BarType.normal ) barColor = Color.blue;
-		if ( pattern[ patternIndex ] == BarType.special ) barColor = Color.red;
-		if ( pattern[ patternIndex ] == BarType.empty ) barColor = Color.yellow;
-	}
-
-	private void AddBeatBar()
-	{
-		GameObject bar = new GameObject( "beatBar", typeof(RectTransform) );
-		bar.AddComponent<CanvasRenderer>();
-		bar.AddComponent<Image>();
-
-		RectTransform rt = bar.GetComponent<RectTransform>();
-		bar.transform.SetParent( canvas.transform );
-		Vector3 p = rt.position;
-
-		rt.transform.position = new Vector3( p.x + Screen.currentResolution.width, p.y, p.z );
-
-		BeatBarBehaviour behaviour = bar.AddComponent<BeatBarBehaviour>();
-		behaviour.barSpeed = barSpeed;
-
-		if( changeOnEveryOther == counter )
-		{
-			counter = 0;
-			++patternIndex;
-			if( patternIndex >= pattern.Count ) patternIndex = 0;
-			changeOnEveryOther = GetCount(pattern[ patternIndex ]);
-			GetPatternType(bar);
-		}
-		else
-		{
-			counter++;
-		}
-		bar.GetComponent<CanvasRenderer>().SetColor(barColor);
-
-		Invoke( "AddBeatBar", 60.0f / BeatsPerMinute );
 	}
 }
