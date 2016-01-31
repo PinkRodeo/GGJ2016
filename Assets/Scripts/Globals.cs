e55da5040b3ddfd1e1b08949999ca5eb14dcf89b
Robin Zaagsma <zaagsma@Robins-MacBook-Pro.local>
2016-01-31 01:07:09 +0100

    *InitScript now tells Global to load poses from an XML file

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -1,12 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
 
 public class Globals {
 
 	private static BirdData[] players;
 	public static Material[] birdMaterials; 
+	public static PoseData[] poses;
 
-	public static void Init()
+	public static void Init(TextAsset poseAsset)
 	{
 		players = new BirdData[4];
 
@@ -14,7 +19,64 @@ public class Globals {
 		for (int ii = 0; ii < 6; ii++) {
 			birdMaterials[ii] = Resources.Load<Material>("Birds/Materials/BirdMat"+(ii+1));
 		}
+		LoadPoses (poseAsset);
 
+		Log.Bobn (poses.Length);
+	}
+
+	private static void LoadPoses(TextAsset posesAsset) {
+		XmlDocument xmlDoc = new XmlDocument(); // xmlDoc is the new xml document.
+
+		xmlDoc.LoadXml(posesAsset.text); // load the file.
+		XmlNodeList poseList = xmlDoc.GetElementsByTagName("pose"); // array of the level nodes.
+
+		poses = new PoseData[poseList.Count];
+		int idx = 0;
+
+		foreach (XmlNode poseInfo in poseList)
+		{
+			PoseData pose = new PoseData();
+			pose.phases = int.Parse (poseInfo.Attributes ["phases"].Value);
+			pose.uiTexture = Resources.Load<Texture2D>(poseInfo.Attributes ["texture"].Value);
+
+			foreach (XmlNode item in poseInfo) { // levels itens nodes.
+				if (item.Name == "leftWing") {
+					pose.leftWing = new float[pose.phases];
+					for (int ii = 0; ii < pose.phases; ii++) {
+						XmlNode dat = item.ChildNodes [ii];
+						pose.leftWing[ii] = int.Parse(dat.Attributes["value"].Value);
+					}
+				}
+				if (item.Name == "rightWing") {
+					pose.rightWing = new float[pose.phases];
+					for (int ii = 0; ii < pose.phases; ii++) {
+						XmlNode dat = item.ChildNodes [ii];
+						pose.rightWing[ii] = int.Parse(dat.Attributes["value"].Value);
+					}
+				}
+				if (item.Name == "head") {
+					pose.head = new Vector2[pose.phases];
+					for (int ii = 0; ii < pose.phases; ii++) {
+						XmlNode dat = item.ChildNodes [ii];
+						Vector2 vec = new Vector2 ();
+						vec.x = float.Parse(dat.Attributes["x"].Value);
+						vec.y = float.Parse(dat.Attributes["y"].Value);
+						pose.head [ii] = vec;
+					}
+				}
+				if (item.Name == "tail") {
+					pose.tail = new Vector2[pose.phases];
+					for (int ii = 0; ii < pose.phases; ii++) {
+						XmlNode dat = item.ChildNodes [ii];
+						Vector2 vec = new Vector2 ();
+						vec.x = float.Parse(dat.Attributes["x"].Value);
+						vec.y = float.Parse(dat.Attributes["y"].Value);
+						pose.tail [ii] = vec;
+					}
+				}
+			}
+			poses [idx++] = pose;
+		}
 	}
 
 	public static void ResetPlayerData()
