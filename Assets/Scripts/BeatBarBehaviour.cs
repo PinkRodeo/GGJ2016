2e8b2b5f93f9716ad8d3e537312eba0036ce213a
Weikie <weikie_yeh@hotmail.com>
2016-01-31 01:34:15 +0100

    *Cleaned up *Fixed spawn position for beats

--- Assets/Scripts/BeatBarBehaviour.cs
+++ Assets/Scripts/BeatBarBehaviour.cs
@@ -2,25 +2,24 @@
 using System.Collections;
 using System;
 
-public class BeatBarBehaviour : MonoBehaviour 
+public class BeatBarBehaviour : MonoBehaviour
 {
 
 	public float barSpeed;
 	public Vector2 barSize;
 
-	void Start () 
+	void Start ()
 	{
-	
+
 	}
 
-	void Update () 
+	void Update ()
 	{
 		RectTransform rt = GetComponent<RectTransform>();
 		//rt.sizeDelta = barSize;
 		rt.transform.Translate( -transform.right * Time.deltaTime * barSpeed );
 		if ( rt.transform.position.x < 0.0 )
 		{
-			Log.Tinas("Should delete now: " + DateTime.Now.ToString("mm:ss:fff") );
 			Destroy(this.gameObject);
 		}
 	}
