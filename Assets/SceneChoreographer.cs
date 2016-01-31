c2894dbeaa727070c1551f70d640480c9d4d1f12
Steff Kempink <mswf@live.nl>
2016-01-31 11:42:56 +0100

    * logo

--- Assets/SceneChoreographer.cs
+++ Assets/SceneChoreographer.cs
@@ -14,6 +14,7 @@ public struct SceneGeographerBalcony
 public class SceneChoreographer : MonoBehaviour
 {
 	public Transform Curtain;
+	public GameObject LogoGameObject;
 
 	public BeatGUIBar beatGUIBar;
 	private StageCamera stageCamera;
