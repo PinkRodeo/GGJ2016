10c3bb2e8da5f9d29ab401be8ff9cdc83e0f1b1c
Weikie <weikie_yeh@hotmail.com>
2016-02-17 13:10:29 +0100

    *Commented out unused to clean warnings

--- Assets/Scripts/Bird/BirdWing.cs
+++ Assets/Scripts/Bird/BirdWing.cs
@@ -5,17 +5,17 @@ public class BirdWing
 
 	private BirdBone shoulder;
 	private BirdBone wing_1;
-	private BirdBone wing_1_f;
+	//private BirdBone wing_1_f;
 
 	private BirdBone wing_2;
 	private BirdBone wing_2_f;
 
 	private BirdBone wing_3;
-	private BirdBone wing_3_f;
+	//private BirdBone wing_3_f;
 
 	private float sign = 1f;
 
-	private float currentApplied = 0f;
+	private float currentApplied;
 
 	private ControllerInput input;
 
@@ -27,19 +27,19 @@ public class BirdWing
 
 		if (suffix == "_L")
 			sign = -1f;
-	
+
 
 		shoulder = BirdBone.CreateBirdBone(shoulderTransform);
 
 		wing_1 = BirdBone.CreateBirdBone(shoulderTransform.FindInChildren("Wing_1" + suffix));
-		
-		wing_1_f = BirdBone.CreateBirdBone(shoulderTransform.FindInChildren("Wing_F_1" + suffix));
-		
+
+		//wing_1_f = BirdBone.CreateBirdBone(shoulderTransform.FindInChildren("Wing_F_1" + suffix));
+
 		wing_2 = BirdBone.CreateBirdBone(shoulderTransform.FindInChildren("Wing_2" + suffix));
 		wing_2_f = BirdBone.CreateBirdBone(shoulderTransform.FindInChildren("Wing_F_2" + suffix));
 
 		wing_3 = BirdBone.CreateBirdBone(shoulderTransform.FindInChildren("Wing_3" + suffix));
-		wing_3_f = BirdBone.CreateBirdBone(shoulderTransform.FindInChildren("Wing_F_3" + suffix));
+		//wing_3_f = BirdBone.CreateBirdBone(shoulderTransform.FindInChildren("Wing_F_3" + suffix));
 	}
 
 	public void Update(float dt)
@@ -51,15 +51,15 @@ public class BirdWing
 		currentApplied = currentInput;
 
 		shoulder.bone.localRotation = shoulder.initialLocalRotation * Quaternion.AngleAxis(Mathf.Lerp(-20f * sign, 0, currentInput), Vector3.up)
-		* Quaternion.AngleAxis(Mathf.Lerp(20f * sign, 0, currentInput), Vector3.forward);
+									  * Quaternion.AngleAxis(Mathf.Lerp(20f * sign, 0, currentInput), Vector3.forward);
 
 
 		wing_1.bone.localRotation = wing_1.initialLocalRotation * Quaternion.AngleAxis(Mathf.Lerp(-80f, 40f, currentInput), Vector3.right);
 
 
 		wing_2.bone.localRotation = wing_2.initialLocalRotation * Quaternion.AngleAxis(Mathf.Lerp(50f* sign, 0, currentInput), Vector3.forward)
-		 *  Quaternion.AngleAxis(Mathf.Lerp(-10f, 0, currentInput), Vector3.right);
-		 
+									*  Quaternion.AngleAxis(Mathf.Lerp(-10f, 0, currentInput), Vector3.right);
+
 		wing_2.bone.localScale = new Vector3(1, Mathf.Lerp(0.7511079f, 1f, currentInput), 1f);
 
 
